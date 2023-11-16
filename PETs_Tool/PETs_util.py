3a5f9903851f58e0d71d6cfbdd63c5dc82714e5f
matheme.justyn <matheme.justyn@gmail.com>
2023-11-16 16:14:22 +0800

    20231116, Justyn: Build SD_SDV and anonymeter expt.

--- PETs_Tool/PETs_util.py
+++ PETs_Tool/PETs_util.py
@@ -40,18 +40,74 @@ def df_downcast(df):
 
 
 
-
 ####### ####### ####### ####### ####### ######
 # label_encoding                             #
 ####### ####### ####### ####### ####### ######
-def label_encoding(__data):
+def label_encoding(df_data):
     from pandas import factorize
-    for col in __data.select_dtypes(['category' ,'object']).columns:
-        if   __data[col].dtype == 'category':
-            __data[col] = __data[col].cat.codes
-        elif __data[col].dtype == 'object':
-            __data[col], _ = factorize(__data[col])
-    return __data
+    for col in df_data.select_dtypes(['category' ,'object']).columns:
+        if   df_data[col].dtype == 'category':
+            df_data[col] = df_data[col].cat.codes
+        elif df_data[col].dtype == 'object':
+            df_data[col], _ = factorize(df_data[col])
+    return df_data
+
+
+
+####### ####### ####### ####### ####### ######
+# Split pandas by P % for N times (sample with replacement) #
+####### ####### ####### ####### ####### ######
+def df_sample(df_data
+             ,sample_ratio=0.8
+             ,random_state=None):
+    __idx_train = df_data.sample(frac         = sample_ratio
+                                ,random_state = random_state).index
+    __idx_validation = df_data.drop(__idx_train).index
+
+    __df_train      = df_data.loc[__idx_train     ].reset_index(drop=True)
+    __df_validation = df_data.loc[__idx_validation].reset_index(drop=True)
+    return __df_train ,__df_validation\
+          ,__idx_train ,__idx_validation
+
+def df_bootstrap(df_data ,params = {}):
+    default_params = {'bootstrap_params' : {'bootstrap_time' : 1
+                                           ,'sample_ratio'   : 0.8
+                                           ,'random_state'   : None
+                                           }
+                     ,'index_params' : {'index_save'      : 'N'
+                                       ,'index_save_data' : 'N'
+                                       ,'index_folder'    : '.\data_dpsd_expt'
+                                       ,'index_filename'  : 'Boostrap'
+                                       }
+                     }
+    params = update_append_nested(default_params ,params)
+
+    from datetime import datetime
+    import pytz
+    __bootstraptime = datetime.now().astimezone(pytz.timezone('Asia/Taipei'))
+
+    dict_idx = {}
+    for __time in range(params['bootstrap_params']['bootstrap_time']):
+        _ ,_ ,__idx_train ,__idx_validation = df_sample(df_data
+                                                       ,params['bootstrap_params']['sample_ratio']
+                                                       ,params['bootstrap_params']['random_state'])
+        dict_idx[__time] = {'idx_train'      : __idx_train
+                           ,'idx_validation' : __idx_validation
+                           }
+    print(f"Bootstrap df: {params['bootstrap_params']['bootstrap_time']} times is done.")
+
+    if params['index_params']['index_save'] == 'Y':
+        import os
+        import pickle
+        if params['index_params']['index_save_data'] == 'Y':
+            dict_idx = {'data' : df_data}
+        index_filename = f"{params['index_params']['index_filename']}_{__bootstraptime.strftime('%Y%m%d_%H%M%S')}_{__bootstraptime.tzinfo.zone.replace('/' ,'_')}"
+        with open(os.path.join(params['index_params']['index_folder'] ,index_filename + ".pkl"), 'wb') as f:
+            pickle.dump(dict_idx ,f)
+        print(f"Bootstrap df: indexes with data save in {index_filename}.pkl.")
+        return dict_idx ,index_filename
+    else:
+        return dict_idx
 
 
 
