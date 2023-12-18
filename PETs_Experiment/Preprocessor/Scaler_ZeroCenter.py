79a92403004cc2d88222464ab721e997b7af4f8a
Alex Chen <alex@AlexdeMacBook-Air.local>
2023-12-18 13:57:30 +0800

    1. Add new Scalers: MinMax and ZeroCenter 2. Modify Scalers: Edit the reshape func 3. Add tests 4. Modify .gitignore

--- PETs_Experiment/Preprocessor/Scaler_ZeroCenter.py
+++ PETs_Experiment/Preprocessor/Scaler_ZeroCenter.py
@@ -2,21 +2,25 @@ from .Scaler import Scaler
 from pandas.api.types import is_numeric_dtype
 from sklearn.preprocessing import StandardScaler
 
+"""
+TODO - Should be merged with Scaler_Standard
+"""
 
-class Scaler_Standard(Scaler):
+
+class Scaler_ZeroCenter(Scaler):
     def __init__(self, df_data, **kwargs):
         super().__init__(df_data, **kwargs)
 
     def handle(self):
         """
-        Scaler - Standardization.
-        Scaling data in DataFrame via Standardization,
-        means removing the mean and scaling to unit variance.
+        Scaler - Zero-Center Scaling.
+        Scaling data in DataFrame via Zero-Center Scaling,
+        the mean of the transformed data will be zero while the variance will be the same.
         Use sklearn.preprocessing.StandardScaler.
 
         ...
         Method:
-            Scaler_Standard(pandas.DataFrame)
+            Scaler_ZeroCenter(pandas.DataFrame)
             Returns:
                 pandas.DataFrame: A pandas DataFrame
                     containing scaling data.
@@ -37,7 +41,7 @@ class Scaler_Standard(Scaler):
         for _col_name in self.scaling_columns_action:
             _col_data = self.df_data[_col_name]
             if is_numeric_dtype(_col_data):
-                self.dict_scaler[_col_name] = StandardScaler()
+                self.dict_scaler[_col_name] = StandardScaler(with_std=False)
                 self.df_data[_col_name] = self.dict_scaler[_col_name].fit_transform(
                     _col_data.values.reshape(-1, 1))
 
