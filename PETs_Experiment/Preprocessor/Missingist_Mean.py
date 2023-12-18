f266ccf769dfdcf1ad175fcec51d3dac479f6733
Alex Chen <alex@AlexdeMacBook-Air.local>
2023-12-18 17:45:35 +0800

    Add Missingist features and tests for ScalerFactory & MissingistFactory

---
+++ PETs_Experiment/Preprocessor/Missingist_Mean.py
@@ -0,0 +1,42 @@
+from .Missingist import Missingist
+
+
+class Missingist_Mean(Missingist):
+    def __init__(self, df_data, **kwargs):
+        super().__init__(df_data, **kwargs)
+
+    def handle(self):
+        """
+        Missing - Fill mean method.
+        Fill the missing values with the mean in DataFrame.
+
+        ...
+        Method:
+            Missingist_Mean(pandas.DataFrame)
+            Returns:
+                pandas.DataFrame: A pandas DataFrame
+                    containing the table with filled missing value, and already re-indexing.
+        ...
+
+        Args:
+
+            df_data (pandas.DataFrame):
+                The pandas DataFrame which might included missing value.
+
+            self.missing_columns_action (list ,optional):
+                Specifies the columns for check missing value.
+        """
+
+        _row_before = self.df_data.isna().any(axis=1).sum()
+        processed = self.df_data.loc[:, self.missing_columns_action].fillna(value=self.df_data.mean()).reset_index(drop=True)
+        _row_filled = _row_before - self.df_data.isna().any(axis=1).sum()
+
+        self.df_data.loc[:, self.missing_columns_action] = processed
+
+        if _row_filled == 0:
+            print(f'Preprocessor - Missingist (Mean): No rows have been filled.')
+        else:
+            print(
+                f'Preprocessor - Missingist (Mean): Filled {_row_filled} rows.')
+
+        return self.df_data
