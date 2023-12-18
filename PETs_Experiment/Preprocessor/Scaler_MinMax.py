19313f890b5062c3239332803c5a7f5679f27bd9
Alex Chen <alex@AlexdeMacBook-Air.local>
2023-12-18 13:57:30 +0800

    1. Add new Scalers: MinMax and ZeroCenter 2. Modify Scalers: Edit the reshape func 3. Add tests 4. Modify .gitignore

--- PETs_Experiment/Preprocessor/Scaler_MinMax.py
+++ PETs_Experiment/Preprocessor/Scaler_MinMax.py
@@ -1,22 +1,22 @@
 from .Scaler import Scaler
 from pandas.api.types import is_numeric_dtype
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 
 
-class Scaler_Standard(Scaler):
+class Scaler_MinMax(Scaler):
     def __init__(self, df_data, **kwargs):
         super().__init__(df_data, **kwargs)
 
     def handle(self):
         """
-        Scaler - Standardization.
-        Scaling data in DataFrame via Standardization,
-        means removing the mean and scaling to unit variance.
-        Use sklearn.preprocessing.StandardScaler.
+        Scaler - Min-Max Scaling.
+        Scaling data in DataFrame via Min-Max Scaling,
+        the range of values will be between 0 and 1.
+        Use sklearn.preprocessing.MinMaxScaler.
 
         ...
         Method:
-            Scaler_Standard(pandas.DataFrame)
+            Scaler_MinMax(pandas.DataFrame)
             Returns:
                 pandas.DataFrame: A pandas DataFrame
                     containing scaling data.
@@ -37,7 +37,7 @@ class Scaler_Standard(Scaler):
         for _col_name in self.scaling_columns_action:
             _col_data = self.df_data[_col_name]
             if is_numeric_dtype(_col_data):
-                self.dict_scaler[_col_name] = StandardScaler()
+                self.dict_scaler[_col_name] = MinMaxScaler()
                 self.df_data[_col_name] = self.dict_scaler[_col_name].fit_transform(
                     _col_data.values.reshape(-1, 1))
 
