f6a23a898179beeb9d5773f29c2caa134fed0c16
Alex Chen <alex@AlexdeMacBook-Air.local>
2023-12-19 18:20:39 +0800

    Fix Missingist

--- PETs_Experiment/Preprocessor/MissingistFactory.py
+++ PETs_Experiment/Preprocessor/MissingistFactory.py
@@ -2,31 +2,53 @@ from .Missingist_Drop import Missingist_Drop
 from .Missingist_Mean import Missingist_Mean
 from .Missingist_Median import Missingist_Median
 from .Missingist_Simple import Missingist_Simple
+import pandas as pd
+
+"""
+TODO - Tidy the code below.
+"""
 
 class MissingistFactory:
     def __init__(self, df_data, **kwargs):
-        try:
-            missing_method = kwargs.get('missing_method', None).lower()
-        except AttributeError:
-            missing_method = kwargs.get('missing_method', None)
-            if isinstance(missing_method, float) or isinstance(missing_method, int):
-                _Missingist = Missingist_Simple(
-                    df_data=df_data, missing_columns_action=kwargs.get('missing_columns_action', None), missing_method=missing_method)
-        else:
-            if missing_method == 'drop':
-                _Missingist = Missingist_Drop(
-                    df_data=df_data, missing_columns_action=kwargs.get('missing_columns_action', None))
-            elif missing_method == 'mean':
-                _Missingist = Missingist_Mean(
-                    df_data=df_data, missing_columns_action=kwargs.get('missing_columns_action', None))
-            elif missing_method == 'median':
-                _Missingist = Missingist_Median(
-                    df_data=df_data, missing_columns_action=kwargs.get('missing_columns_action', None))
-            else:
-                raise ValueError(
-                    f"Preprocessor - MissingistFactory: missing_method {missing_method} didn't support.")
 
-        self.Missingist = _Missingist
+        self.missing_dict = {}
+        self.missing_col = kwargs.get('missing_columns_action', None)
+        self.df = df_data
+
+        try:
+            self.missing_method = kwargs.get('missing_method', None).lower()
+        except AttributeError:
+            self.missing_method = kwargs.get('missing_method', None)
+        except:
+            raise ValueError(
+                    f"Preprocessor - MissingistFactory: missing_method {self.missing_method} didn't support.")
+
+        if self.missing_col is not None:
+            for col in self.missing_col:
+                if self.missing_method == 'drop':
+                    _Missingist = Missingist_Drop(
+                        df_data=df_data[col])
+                elif self.missing_method == 'mean':
+                    _Missingist = Missingist_Mean(
+                        df_data=df_data[col])
+                elif self.missing_method == 'median':
+                    _Missingist = Missingist_Median(
+                        df_data=df_data[col])
+                elif isinstance(self.missing_method, float) or isinstance(self.missing_method, int):
+                    _Missingist = Missingist_Simple(
+                        df_data=df_data[col], missing_method=self.missing_method)
+                else:
+                    raise ValueError(
+                            f"Preprocessor - MissingistFactory: missing_method {self.missing_method} didn't support.")
+
+                self.missing_dict[col] = _Missingist
 
     def handle(self):  # -> pd.DataFrame
-        return self.Missingist.handle()
+        processed_df = self.df.copy()
+        for col in self.missing_col:
+            handled_data = self.missing_dict[col].handle()
+            processed_df[col] = handled_data.values
+
+        if self.missing_method == 'drop':
+            processed_df.dropna(inplace=True)
+        return processed_df, self.missing_dict
--- tests/Preprocessor/test_Missingist_Drop.py
+++ tests/Preprocessor/test_Missingist_Drop.py
@@ -5,8 +5,8 @@ from PETs_Experiment.Preprocessor.Missingist_Drop import Missingist_Drop
 class TestMissingistDrop:
     def test_drop_no_missing_values(self):
         # Prepare test data
-        df_data = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
-        df_data_setting = {'missing_method': 'drop' ,'missing_columns': ['A', 'B'], 'missing_columns_action': ['A', 'B']}
+        df_data = pd.DataFrame({'A': [1, 2, 3]})
+        df_data_setting = {'missing_method': 'drop', 'missing_columns_action': 'A'}
         
         # Create an instance of the class
         obj = Missingist_Drop(df_data, **df_data_setting)
@@ -15,22 +15,18 @@ class TestMissingistDrop:
         result = obj.handle()
         
         # Assert the result
-        assert result.shape[0] == 3
-        assert 'A' in result.columns
-        assert 'B' in result.columns
+        assert result.shape == (3, 1)
     
     def test_drop_with_missing_values(self):
         # Prepare test data
-        df_data = pd.DataFrame({'A': [1, None, 3], 'B': [4, 6, None]})
-        df_data_setting = {'missing_method': 'drop' ,'missing_columns': ['A', 'B'], 'missing_columns_action': ['A', 'B']}
+        df_data = pd.DataFrame({'A': [1, None, 3]})
+        df_data_setting = {'missing_method': 'drop', 'missing_columns_action': 'A'}
         
         # Create an instance of the class
-        obj = Missingist_Drop(df_data, **df_data_setting)
+        obj = Missingist_Drop(df_data['A'], **df_data_setting)
         
         # Call the method to be tested
         result = obj.handle()
         
         # Assert the result
-        assert result.shape[0] == 1
-        assert 'A' in result.columns
-        assert 'B' in result.columns
+        assert result.shape == (3,) # because Missingist_Drop.handle do nothing
