1012850369428b0c367d353a43af7c6b4afacf2c
matheme-justyn <matheme.justyn@gmail.com>
2023-12-22 08:33:26 +0800

    Merge pull request #48 from matheme-justyn/27-class-executor
    sync w/ Alex: add output_file_path para. in SDV sample

--- PETs_Experiment/Synthesizer/SDV/SDV_SingleTableFactory.py
+++ PETs_Experiment/Synthesizer/SDV/SDV_SingleTableFactory.py
@@ -1,6 +1,11 @@
 class SDV_SingleTableFactory:
     def __init__(self, data, **kwargs):
         synthesizing_method = kwargs.get('synthesizing_method', None)
+        # _para = {
+        #     'sample_num_rows': kwargs.get('sample_num_rows', None)
+        #    ,'sample_batch_size': kwargs.get('sample_batch_size', None)
+        # }
+        #  ,**_para
 
         if   synthesizing_method == 'sdv-singletable-coupulagan':
             from .SDV_SingleTable_CoupulaGAN import SDV_SingleTable_CoupulaGAN
@@ -13,7 +18,7 @@ class SDV_SingleTableFactory:
             _Synthesizer = SDV_SingleTable_GaussianCoupula(data=data)
         elif synthesizing_method == 'sdv-singletable-tvae':
             from .SDV_SingleTable_TVAE import SDV_SingleTable_TVAE
-            _Synthesizer = SDV_SingleTable_TVAE(data=dataa)
+            _Synthesizer = SDV_SingleTable_TVAE(data=data)
         else:
             raise ValueError(
                 f"Synthesizer (SDV - SDV_SingleTableFactory): synthesizing_method {synthesizing_method} didn't support.")
@@ -26,9 +31,3 @@ class SDV_SingleTableFactory:
 
 
 
-
-        # _para = {
-        #     'sample_num_rows': kwargs.get('sample_num_rows', None)
-        #    ,'sample_batch_size': kwargs.get('sample_batch_size', None)
-        # }
-        #  ,**_para
