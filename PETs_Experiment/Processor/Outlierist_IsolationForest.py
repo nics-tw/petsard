5162b9c426b7b16e4e36e25378a96bd62405a8f6
Alex Chen <alex@AlexdeMacBook-Air.local>
2023-12-25 15:23:57 +0800

    Refactor starts

---
+++ PETs_Experiment/Processor/Outlierist_IsolationForest.py
@@ -0,0 +1,65 @@
+from .Outlierist import Outlierist
+from pandas import to_datetime
+from pandas.api.types import is_datetime64_any_dtype
+from sklearn.ensemble import IsolationForest
+
+"""
+TODO - Because Isolation Forest detects outliers from the WHOLE dataset, any transformation (e.g., transfrom timestamp to numerical data) should be done before the preprocessing.
+"""
+
+class Outlierist_IsolationForest(Outlierist):
+    def __init__(self, df_data, **kwargs):
+        super().__init__(df_data, **kwargs)
+
+    def handle(self):
+        """
+        Outlier - Isolation Forest method.
+        Drop the samples identified as outliers by Isolation Forest.
+
+        ...
+        Method:
+            Outlierist_IsolationForest(pandas.DataFrame)
+            Returns:
+                pandas.DataFrame: A pandas DataFrame
+                    containing drop outlier data, and already re-indexing.
+        ...
+
+        Args:
+
+            df_data (pandas.DataFrame):
+                The pandas DataFrame which might included missing value.
+
+            outlier_columns_action (list ,optional):
+                Specifies the columns for check outlier value.
+        """
+
+        _row_ori = self.df_data.shape[0]
+        _digits_row_ori = len(str(_row_ori))
+        
+        transformed_data = self.df_data.copy()
+
+        for _col_name in self.outlier_columns_action:
+            _col_data = transformed_data[_col_name]
+            if is_datetime64_any_dtype(_col_data):
+
+                _col_data_timestamp = to_datetime(
+                    _col_data).view(int) / 10**9
+                
+                transformed_data[_col_name] = _col_data_timestamp
+
+        isof = IsolationForest()
+
+        outlier_vector = isof.fit_predict(transformed_data[self.outlier_columns_action])
+
+
+
+        _row_drop_ttl = sum(outlier_vector == -1)
+        if _row_drop_ttl == 0:
+            print(f'Preprocessor - Outlierist (Isolation Forest): None of rows have been dropped.')
+        else:
+            print(
+                f'Preprocessor - Outlierist (Isolation Forest): Totally Dropped {_row_drop_ttl: >{_digits_row_ori}} in {_row_ori} rows.')
+
+        self.df_data = self.df_data.loc[outlier_vector == 1].reset_index(drop=True)
+
+        return self.df_data
---
+++ tests/Processor/test_UniformEncoder.py
@@ -0,0 +1,58 @@
+import pytest
+from PETs_Experiment.Processor.Encoder import Encoder_Uniform
+import pandas as pd
+import numpy as np
+from PETs_Experiment.Error import UnfittedError
+
+class Test_Encoder_Uniform:
+    def test_Encoder_Uniform(self):
+        # Prepare test data
+        df_data1 = ['A'] * 7 + ['B'] * 3 + ['C'] * 5 + ['D'] * 5
+        df_data2 = [0] * 20
+        df_data = pd.DataFrame({'col1': pd.Categorical(df_data1), 'col2': df_data2})
+
+        # Data for triggering error
+        df_data1_error = ['A'] * 7 + ['B'] * 3 + ['E'] * 5 + ['D'] * 5
+        df_data_error = pd.DataFrame({'col1': df_data1_error, 'col2': df_data2})
+
+        # Create an instance of the class
+        ue = Encoder_Uniform()
+
+        with pytest.raises(UnfittedError):
+            ue.transform(df_data)
+
+        with pytest.raises(UnfittedError):
+            ue.inverse_transform(df_data)
+
+        # Call the method to be tested
+        ue.fit(df_data['col1'])
+        
+        assert set(ue.labels) == set(['A', 'B', 'C', 'D']) # type: ignore
+        assert len(ue.lower_values) == 4 # type: ignore
+        assert len(ue.upper_values) == 4 # type: ignore
+        assert ue.lower_values[2] == ue.upper_values[1] # type: ignore
+
+        with pytest.raises(ValueError):
+            ue.transform(df_data_error['col1'])
+
+        transformed = ue.transform(df_data['col1'])
+
+        print(transformed)
+
+        assert type(transformed) == pd.Series
+        assert transformed.dtypes == np.float64
+        assert transformed.min() >= 0
+        assert transformed.max() <= 1
+
+        # Data for triggering error
+        transformed_error = transformed.copy()
+        transformed_error[0] = 2.9
+
+        with pytest.raises(ValueError):
+            ue.inverse_transform(transformed_error)
+
+        rtransformed = ue.inverse_transform(transformed)
+
+        assert list(rtransformed.values) == list(df_data['col1'].values)
+
+    
