4b1ec090159a3020b6f6ee81274f4c34d205d043
Alex Chen <alex@AlexdeMacBook-Air.local>
2023-12-27 12:04:45 +0800

    Rename as Hyperprocessor

--- PETsARD/__init__.py
+++ PETsARD/__init__.py
@@ -37,7 +37,7 @@ from .Processor import *
 
 from .Executor import Executor
 
-from .Processor_Manager import Processor_Manager
+from .HyperProcessor import Processor_Manager
 
 __all__ =   ['Loader'
                 ,'Splitter'
@@ -51,5 +51,5 @@ __all__ =   ['Loader'
             ,'Executor'
             ,'Splitter'
             
-                ,'Processor_Manager'
+                ,'HyperProcessor'
             ]
