3c6bf8b653339e9b0aa85d7dcca900b59049b8e7
matheme-justyn <matheme.justyn@gmail.com>
2024-01-04 17:55:59 +0800

    Syn - SDV parts

--- PETsARD/Synthesizer/SDV/SDV.py
+++ PETsARD/Synthesizer/SDV/SDV.py
@@ -1,4 +1,21 @@
+import pandas as pd
+
+
 class SDV():
-    def __init__(self, data, **kwargs):
-        self.data = data
-        pass
+    """
+    Base class for all "SDV".
+
+    The "SDV" class defines the common API
+    that all the "SDV" need to implement, as well as common functionality.
+
+    Args:
+        data (pd.DataFrame): The data to be synthesized.
+        **kwargs: The other parameters.
+
+    Return:
+        None
+    """
+
+    def __init__(self, data: pd.DataFrame, **kwargs):
+        self.data: pd.DataFrame = data
+        self.syn_method: str = 'Unknown'
