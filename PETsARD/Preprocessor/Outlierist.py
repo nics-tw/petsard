02bab7e28de26e6d265e4d92d4ce937a74a78bbd
matheme-justyn <matheme.justyn@gmail.com>
2024-01-05 10:28:09 +0800

    Pre/Post parts

--- PETsARD/Preprocessor/Outlierist.py
+++ PETsARD/Preprocessor/Outlierist.py
@@ -2,9 +2,10 @@ class Outlierist:
     def __init__(self, df_data, **kwargs
                  ):
         self.df_data = df_data
-        self.outlier_columns_action = kwargs.get(
-            'outlier_columns_action', None)
+        self.outlier_columns_action = \
+            kwargs.get('outlier_columns_action', None)
 
     def handle(self, **kwargs):
         raise NotImplementedError(
-            "This method should be implemented by subclasses.")
+            "This method should be implemented by subclasses."
+        )
