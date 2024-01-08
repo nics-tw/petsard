15c9afb5c2efaead507ee69281bc16c57d32d390
matheme-justyn <matheme.justyn@gmail.com>
2024-01-08 13:03:48 +0800

    240108 - Commit the Changes

--- PETsARD/Loader/__init__.py
+++ PETsARD/Loader/__init__.py
@@ -1,7 +1,4 @@
 from .Loader import Loader
-from .LoaderFactory import LoaderFactory
-from .Loader_csv_pandas import Loader_csv_pandas
-from .Loader_excel_pandas import Loader_excel_pandas
 from .Splitter import Splitter
 
 
