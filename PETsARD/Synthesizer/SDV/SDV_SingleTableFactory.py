bd4edf49b3d3dddffcead75d985d3bf74b3d2708
matheme-justyn <matheme.justyn@gmail.com>
2024-01-10 07:38:37 +0800

    20240110 - Resolved conflict

--- PETsARD/Synthesizer/SDV/SDV_SingleTableFactory.py
+++ PETsARD/Synthesizer/SDV/SDV_SingleTableFactory.py
@@ -10,13 +10,17 @@ class SDV_SingleTableFactory:
     def __init__(self, data: pd.DataFrame, **kwargs) -> None:
         """
         Base class for all "SDV".
+
         The "SDV" class defines the common API
         that all the "SDV" need to implement, as well as common functionality.
+
         Args:
             data (pd.DataFrame): The data to be synthesized.
             **kwargs: The other parameters.
+
         Return:
             None
+
         TODO As AnonymeterMethodMap, use class define mapping of string and int,
              don't use string condition.
         """
--- PETsARD/Synthesizer/SDV/SDV_SingleTable_TVAE.py
+++ PETsARD/Synthesizer/SDV/SDV_SingleTable_TVAE.py
@@ -2,15 +2,22 @@ import pandas as pd
 from sdv.single_table import TVAESynthesizer
 
 from .SDV_SingleTable import SDV_SingleTable
-from sdv.single_table import TVAESynthesizer
-import pandas as pd
 
 
 class SDV_SingleTable_TVAE(SDV_SingleTable):
-    def __init__(self,   data, **kwargs):
+    """
+    Implement TVAE synthesize method.
+
+    Args:
+        data (pd.DataFrame): The data to be synthesized.
+        **kwargs: The other parameters.
+
+    Return:
+        None
+    """
+
+    def __init__(self, data: pd.DataFrame, **kwargs):
         super().__init__(data, **kwargs)
         self.syn_method: str = 'TVAE'
-        self._syn_method: str = 'TVAE'
 
-        # metadata already create in SDV_SingleTable
         self._Synthesizer = TVAESynthesizer(self.metadata)
