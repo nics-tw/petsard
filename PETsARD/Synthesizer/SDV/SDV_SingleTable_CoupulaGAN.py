124016538cf6430c39983a80f28521ed9989e585
matheme-justyn <matheme.justyn@gmail.com>
2024-01-10 07:41:15 +0800

    Revert "Eval parts (w/o Anonymeter)"

--- PETsARD/Synthesizer/SDV/SDV_SingleTable_CoupulaGAN.py
+++ PETsARD/Synthesizer/SDV/SDV_SingleTable_CoupulaGAN.py
@@ -1,22 +1,10 @@
 from .SDV_SingleTable import SDV_SingleTable
-from sdv.single_table import CopulaGANSynthesizer
-import pandas as pd
-
 
 class SDV_SingleTable_CoupulaGAN(SDV_SingleTable):
-    """
-    Implement CopulaGAN synthesize method.
-
-    Args:
-        data (pd.DataFrame): The data to be synthesized.
-        **kwargs: The other parameters.
-
-    Return:
-        None
-    """
-    def __init__(self, data: pd.DataFrame, **kwargs) -> None:
+    def __init__(self,   data, **kwargs):
         super().__init__(data, **kwargs)
-        self._syn_method: str = 'CoupulaGAN'
+        self._syn_method = 'CoupulaGAN'
 
         # metadata already create in SDV_SingleTable
+        from sdv.single_table import CopulaGANSynthesizer
         self._Synthesizer = CopulaGANSynthesizer(self.metadata)
