6111fa33402b51d66a9ad7bc7ab9254b694a56a0
Alex Chen <alex@AlexdeMacBook-Air.local>
2024-01-23 17:12:05 +0800

    Create SmartNoise folder

--- PETsARD/Synthesizer/SynthesizerFactory.py
+++ PETsARD/Synthesizer/SynthesizerFactory.py
@@ -1,16 +1,23 @@
 import pandas as pd
 
 from PETsARD.Synthesizer.SDV.SDVFactory import SDVFactory
+from PETsARD.Synthesizer.SmartNoise.SmartNoiseFactory import SmartNoiseFactory
 
 
 class SynthesizerFactory:
     def __init__(self, data, **kwargs):
         synthesizing_method = kwargs.get('synthesizing_method', None)
+        eps: float = kwargs.get('epsilon', 5.0)
 
         if synthesizing_method.startswith('sdv'):
             _Synthesizer = SDVFactory(data=data, 
                                       synthesizing_method=synthesizing_method
                                       ).create_synthesizer()
+        elif synthesizing_method.startswith('smartnoise'):
+            _Synthesizer = SmartNoiseFactory(data=data,
+                                synthesizing_method=synthesizing_method,
+                                epsilon=eps
+                                ).create_synthesizer()
         else:
             raise ValueError(
                 f"Synthesizer - SynthesizerFactory: "
