da9f182671d50802ac953ec6c1af43f8e41892e2
Alex Chen <alex@AlexdeMacBook-Air.local>
2024-01-26 11:41:46 +0800

    Add new files

---
+++ PETsARD/synthesizer/sdv/sdv_singletable_tvae.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sdv.single_table import TVAESynthesizer
+
+from PETsARD.synthesizer.sdv.sdv_singletable import SDV_SingleTable
+
+
+class SDV_SingleTable_TVAE(SDV_SingleTable):
+    """
+    Implement TVAE synthesize method.
+
+    Args:
+        data (pd.DataFrame): The data to be synthesized.
+        **kwargs: The other parameters.
+
+    Return:
+        None
+    """
+
+    def __init__(self, data: pd.DataFrame, **kwargs):
+        super().__init__(data, **kwargs)
+        self.syn_method: str = 'TVAE'
+
+        self._Synthesizer = TVAESynthesizer(self.metadata)
