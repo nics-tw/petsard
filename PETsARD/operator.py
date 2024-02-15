0e80b4d621b4c45d0d339e5cf4d1cd0b4f7b77bc
matheme-justyn <matheme.justyn@gmail.com>
2024-02-15 10:29:28 +0800

    20240215, ProcessorOperator

--- PETsARD/operator.py
+++ PETsARD/operator.py
@@ -1,6 +1,34 @@
+from typing import (
+    Dict, List, Optional
+)
+
 import pandas as pd
 
-from PETsARD.loader import Loader
+from PETsARD.loader import Loader, Splitter
+from PETsARD.processor import Processor
+
+
+class MinMetadata:
+    """
+    Initializes an instance of the minimum available Metadata class for Operator use.
+    """
+
+    def __init__(self, config: dict):
+        """
+        Args:
+            config (dict): A dictionary containing configuration information for various modules.
+
+        Attributes:
+            metadata (dict): A dictionary containing metadata information.
+        """
+        col = set()
+        for module in ['encoder', 'missing', 'outlier', 'scaler']:
+            if module in config:
+                col.update(list(config[module].keys()))
+        self.metadata = {
+            'col': {key: {} for key in col},
+            'global': {}
+        }
 
 
 class Operator:
@@ -8,15 +36,25 @@ class Operator:
     The interface of the objects used by Executor.run()
     """
     def __init__(self, config: dict):
+        """
+        Args:
+            config (dict):
+                A dictionary containing configuration parameters.
+        """
         pass
 
-    def run(self):
+    def run(self, input: dict):
         """
         Execute the module's functionality.
+
+        Args:
+            input (dict):
+                A input dictionary contains module required input from Status.
+                See Status.get_input_from_prev(module) for more details.
         """
         raise NotImplementedError
 
-    def get_result(self, tag: str = None):
+    def get_result(self):
         """
         Retrieve the result of the module's operation,
             as data storage varies between modules.
@@ -46,18 +84,72 @@ class LoaderOperator(Operator):
         super().__init__(config)
         self.loader = Loader(**config)
 
-    def run(self):
+    def run(self, input: dict=None):
         """
         Executes the data loading process using the Loader instance.
+
+        Args:
+            input (dict): Loader input should contains nothing ({}).
         """
         self.loader.load()
 
-    def get_result(self, tag: str=None) -> pd.DataFrame:
+    def get_result(self) -> pd.DataFrame:
         """
         Retrieve the loading result.
-        ...
-        Args
-            tag (str)
-                Inherited from Operator. Not applicable.
         """
         return self.loader.data
+    
+
+class SplitterOperator(Operator):
+    """
+    SplitterOperator is responsible for splitting data
+        using the configured Loader instance as a decorator.
+    """
+
+    def __init__(self, config: dict):
+        super().__init__(config)
+        self.splitter = Splitter(**config)
+
+    def run(self, input: dict):
+        """
+        Executes the data splitting process using the Splitter instance.
+
+        Args:
+            input (dict): Splitter input should contains data (pd.DataFrame) and exclude_index (list).
+        """
+        self.splitter.split(**input)
+
+    def get_result(self) -> pd.DataFrame:
+        """
+        Retrieve the splitting result.
+            Due to Config force num_samples = 1, return 1st dataset is fine.
+        """
+        return self.splitter.data[1]
+    
+
+class ProcessorOperator(Operator):
+    """
+    ProcessorOperator is responsible for pre-processing data
+        using the configured Processor instance as a decorator.
+    """
+
+    def __init__(self, config: dict):
+        super().__init__(config)
+        self.processor = Processor(
+            metadata=MinMetadata(config=config),
+            config=config
+        )
+
+    def run(self, input: dict):
+        """
+        Executes the data pre-process using the Processor instance.
+        """
+        self.processor.fit(**input)
+        self.processor.data_preproc: pd.DataFrame = \
+            self.processor.transform(**input)
+
+    def get_result(self) -> pd.DataFrame:
+        """
+        Retrieve the pre-processing result.
+        """
+        return self.processor.data_preproc
