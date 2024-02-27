2d4fa8a6667a16251d6c26b222d3e4d3be2364e3
matheme-justyn <matheme.justyn@gmail.com>
2024-02-27 18:36:51 +0800

    20240227, Splitter support method = custom_data

--- PETsARD/loader/splitter.py
+++ PETsARD/loader/splitter.py
@@ -3,6 +3,7 @@ from typing import Dict, List, Optional, Union
 
 import pandas as pd
 
+from PETsARD.loader.loader import Loader # avoid circular import
 from PETsARD.error import ConfigError
 
 
@@ -12,49 +13,95 @@ class Splitter:
     a.) split input data via assigned ratio (train_split_ratio)
     b.) resampling assigned times (num_samples)
     c.) output their train/validation indexes (self.index_samples) and pd.DataFrame data (self.data)
+
+    When method is 'custom_data', the data will be loaded from the filepath.
     """
 
     def __init__(
         self,
+        method: str = None,
+        filepath: Optional[Dict[str, str]] = None,
         num_samples:       int = 1,
         train_split_ratio: float = 0.8,
-        random_state:      Optional[Union[int, float, str]] = None
+        random_state:      Optional[Union[int, float, str]] = None,
+        **kwargs
     ):
         """
         Args:
+            method (str):
+                Support load exist split data, accepted 'custom_data' only.
+                Default is None.
+            filepath (Dict[str, str]):
+                The filepath of the data to be loaded.
+                Only required when method = 'custom_data'. Default is None.
             num_samples (int): Number of times to resample the data. Default is 1.
             train_split_ratio (float): Ratio of data to assign to the training set, must between 0 ~ 1. Default is 0.8.
             random_state (int | float | str): Seed for random number generation. Default is None.
         """
-        if not (0 <= train_split_ratio <= 1):
-            raise ConfigError(
-                "Splitter:  train_split_ratio must be a float between 0 and 1.")
-        self.num_samples = num_samples
-        self.train_split_ratio = train_split_ratio
-        self.random_state = random_state
+        if method:
+            if method.lower() != 'custom_data':
+                raise ConfigError
+            else:
+                if filepath is None:
+                    raise ConfigError
+                if 'ori' not in filepath\
+                    or 'control' not in filepath:
+                    raise ConfigError
+
+                config = kwargs
+                self.loader: dict = {}
+
+                for key in ['ori', 'control']:
+                    config['filepath'] = filepath[key]
+                    self.loader[key] = Loader(**config)
+
+                config['method'] = method
+                config['filepath'] = filepath
+                self.config = config
+
+        else:
+            if not (0 <= train_split_ratio <= 1):
+                raise ConfigError(
+                    "Splitter:  train_split_ratio must be a float between 0 and 1.")
+            self.config = {
+                'num_samples': num_samples,
+                'train_split_ratio': train_split_ratio,
+                'random_state': random_state,
+            }
 
     def split(self, data: pd.DataFrame, exclude_index: List[int] = None):
         """
         Perform index bootstrapping on the Splitter-initialized data
             and split it into train and validation sets using the generated index samples.
-        
+
+        When method is 'custom_data', the data will be loaded from the filepath.
+
         Args:
             data (pd.DataFrame): The dataset which wait for split.
             exclude_index (List[int]): The exist index we want to exclude them from our sampling.
         """
-        data.reset_index(drop=True, inplace=True)  # avoid unexpected index
-
-        self.index = self._index_bootstrapping(
-            index=data.index.tolist(),
-            exclude_index=exclude_index
-        )
-
         self.data: dict = {}
-        for key, index in self.index.items():
-            self.data[key] = {
-                'train': data.iloc[index['train']].reset_index(drop=True),
-                'validation': data.iloc[index['validation']].reset_index(drop=True)
+
+        if 'method' in self.config:
+            self.loader['ori'].load()
+            self.loader['control'].load()
+            self.data[1] = {
+                'train': self.loader['ori'].data,
+                'validation': self.loader['control'].data
             }
+        else:
+            data.reset_index(drop=True, inplace=True)  # avoid unexpected index
+
+            self.index = self._index_bootstrapping(
+                index=data.index.tolist(),
+                exclude_index=exclude_index
+            )
+
+            for key, index in self.index.items():
+                self.data[key] = {
+                    'train': data.iloc[index['train']].reset_index(drop=True),
+                    'validation': data.iloc[index['validation']].reset_index(drop=True)
+                }
 
     def _index_bootstrapping(
         self, index: list, exclude_index: List[int] = None) -> Dict[int, List[int]]:
@@ -67,10 +114,10 @@ class Splitter:
             exist_index (Dict[int, List[int]])
                 same as split()
         """
-        if self.random_state is not None:
-            random.seed(self.random_state)
+        if self.config['random_state'] is not None:
+            random.seed(self.config['random_state'])
 
-        sample_size = int(len(index) * self.train_split_ratio)
+        sample_size = int(len(index) * self.config['train_split_ratio'])
 
         sampled_seen = set()
         if exclude_index:  # external samples seen\
@@ -78,8 +125,8 @@ class Splitter:
 
         sampled_index = {}
         # assume max sampling time as num_sample.
-        maxattempts = self.num_samples
-        for n in range(self.num_samples):
+        maxattempts = self.config['num_samples']
+        for n in range(self.config['num_samples']):
             # re-calculate when success.
             attempts = 0
             while attempts < maxattempts:
@@ -99,8 +146,8 @@ class Splitter:
                 if attempts == maxattempts:
                     raise ConfigError(
                         f"Splitter: "
-                        f"Unable to sample {self.num_samples} pairs of index "
-                        f"with a ratio of {self.train_split_ratio} "
+                        f"Unable to sample {self.config['num_samples']} pairs of index "
+                        f"with a ratio of {self.config['train_split_ratio']} "
                         f"within {maxattempts} attempts due to collisions.\n"
                         f"Please review your data size "
                         f"and choose a suitable sampling ratio."
