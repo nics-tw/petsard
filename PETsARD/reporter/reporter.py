fb4fd3a181f5cceb74faf534f14478a8565d4414
matheme-justyn <matheme.justyn@gmail.com>
2024-02-29 18:59:49 +0800

    20240229, Reporter and ReporterSaveData docstring

--- PETsARD/reporter/reporter.py
+++ PETsARD/reporter/reporter.py
@@ -28,7 +28,20 @@ class ReporterMap():
 
 
 class Reporter:
+    """
+    A class that represents a reporter.
+    """
+
     def __init__(self, method: str, **kwargs):
+        """
+        Args:
+            method (str): The method used for reporting.
+            **kwargs: Additional configuration parameters.
+
+        Attributes:
+            config (dict): A dictionary containing the configuration parameters.
+            reporter (object): An object representing the specific reporter based on the method.
+        """
         self.config = kwargs
         self.config['method'] = method.lower()
         self.reporter = None
@@ -43,14 +56,35 @@ class Reporter:
             raise UnsupportedMethodError
 
     def create(self, data: dict) -> None:
+        """
+        Creates a report using the specified data.
+
+        Args:
+            data (dict): The data used for creating the report.
+        """
         self.reporter.create(data=data)
 
     def report(self) -> None:
+        """
+        Generates and saves the report.
+        """
         self.reporter.report()
 
 
 class ReportBase(ABC):
+    """
+    Base class for reporting data.
+    """
+
     def __init__(self, config: dict):
+        """
+        Args:
+            config (dict): Configuration settings for the report.
+
+        Attributes:
+            config (dict): Configuration settings for the report.
+            data (dict): Data for the report.
+        """
         self.config: dict = config
         if 'method' not in self.config:
             raise ConfigError
@@ -61,13 +95,29 @@ class ReportBase(ABC):
 
     @abstractmethod
     def create(self, data: dict) -> None:
+        """
+        Abstract method for creating the report.
+
+        Args:
+            data (dict): The data used for creating the report.
+        """
         raise NotImplementedError
 
     @abstractmethod
     def report(self) -> None:
+        """
+        Abstract method for reporting the data.
+        """
         raise NotImplementedError
 
     def _save(self, data: pd.DataFrame, full_output: str) -> None:
+        """
+        Save the data to a CSV file.
+
+        Args:
+            data (pd.DataFrame): The data to be saved.
+            full_output (str): The full output path for the CSV file.
+        """
         print(f"Now is {full_output} save to csv...")
         data.to_csv(
             path_or_buf=f"{full_output}.csv",
@@ -77,7 +127,19 @@ class ReportBase(ABC):
 
 
 class ReportSaveData(ReportBase):
+    """
+    Save raw/processed data to file.
+    """
+
     def __init__(self, config: dict):
+        """
+        Args:
+            config (dict): The configuration dictionary.
+
+        Raises:
+            ConfigError: If the 'source' key is missing in the config
+                or if the value of 'source' is not a string or a list of strings.
+        """
         super().__init__(config)
 
         # Source should be string or list of string: Union[str, List[str]]
@@ -94,6 +156,15 @@ class ReportSaveData(ReportBase):
             self.config['source'] = [self.config['source']]
 
     def create(self, data: dict) -> None:
+        """
+        Creates the report data.
+
+        Args:
+            data (dict): The data dictionary.
+
+        Raises:
+            ConfigError: If the index tuple is not an even number.
+        """
         # last 1 of index shoulde remove postifx "_[xxx]" to match source
         pattern = re.compile(r'_(\[[^\]]*\])$')
         for index, df in data.items():
@@ -111,6 +182,12 @@ class ReportSaveData(ReportBase):
                 self.data[full_expt] = df
 
     def report(self) -> None:
+        """
+        Generates the report.
+
+        Notes:
+            Some of the data may be None, such as Evaluator.get_global/columnwise/pairwise. These will be skipped.
+        """
         for expt_name, df in self.data.items():
             # Some of the data may be None.
             #   e.g. Evaluator.get_global/columnwise/pairwise
