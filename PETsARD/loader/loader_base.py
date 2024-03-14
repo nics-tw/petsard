e16e5040f7e39b8cb55340342a4839984ec7c417
matheme-justyn <matheme.justyn@gmail.com>
2024-03-14 08:15:46 +0800

    20240314, config in loader_base

--- PETsARD/loader/loader_base.py
+++ PETsARD/loader/loader_base.py
@@ -8,11 +8,17 @@ class LoaderBase(ABC):
 
         The "Loader" class defines the common API
         that all the "Loader" need to implement, as well as common functionality.
-
     """
 
-    def __init__(self, para_Loader: dict):
-        self.para_Loader = para_Loader
+    def __init__(self, config: dict):
+        """
+        Args:
+            config (dict): The configuration for the loader modules.
+
+        Attr:
+            config (dict): The configuration for the loader modules.
+        """
+        self.config = config
 
     @abstractmethod
     def load(self):
