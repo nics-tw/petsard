bb47c8b824873f85afb155f2cf6deab4e65e0b68
matheme-justyn <matheme.justyn@gmail.com>
2024-03-14 15:41:29 +0800

    20240314, fixed sha256 of acs-person

--- PETsARD/loader/util.py
+++ PETsARD/loader/util.py
@@ -1,3 +1,4 @@
+import hashlib
 from typing import Dict
 import warnings
 
@@ -38,6 +39,23 @@ def verify_column_types(column_types: Dict[str, list] = None) -> bool:
         coltype.lower() in ALLOWED_COLUMN_TYPES
         for coltype in column_types.keys()
     )
+
+
+def DigestSha256(filepath):
+    """
+    Calculate SHA-256 value of file. Load 128KB at one time.
+    ...
+    Args:
+        filepath (str) Openable file full path.
+    ...
+    return:
+        (str) SHA-256 value of file.
+    """
+    sha256hash = hashlib.sha256()
+    with open(filepath, "rb") as f:
+        for byte_block in iter(lambda: f.read(131072), b""):
+            sha256hash.update(byte_block)
+    return sha256hash.hexdigest()
 
 
 def optimize_dtypes(
