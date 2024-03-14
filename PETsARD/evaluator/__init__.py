f5f2009b8a95cca449a84a2a0364b51571f05b32
Alex Chen <alex@AlexdeMacBook-Air.local>
2024-03-14 11:04:15 +0800

    register new module

--- PETsARD/evaluator/__init__.py
+++ PETsARD/evaluator/__init__.py
@@ -1,7 +1,9 @@
 from PETsARD.evaluator.evaluator import Evaluator
 from PETsARD.evaluator.describer import Describer
+from PETsARD.evaluator.automl import AutoML
 
 __all__ = [
     'Evaluator',
-    'Describer'
+    'Describer',
+    'AutoML'
 ]
