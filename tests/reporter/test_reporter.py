275afc8326fe593fc42ce9259827e65764677d47
matheme-justyn <matheme.justyn@gmail.com>
2024-03-26 13:26:30 +0800

    20240326, test_eval

--- tests/reporter/test_reporter.py
+++ tests/reporter/test_reporter.py
@@ -51,15 +51,17 @@ class Test_Reporter:
         """
         Test case for the arg. `method` = 'save_report' of Reporter class.
 
+        - The Reporter will be created when:
+            - method='save_report', granularity='global', but no eval is provided
         - The Reporter will raise an UnsupportedMethodError when:
             - method='save_report' but no granularity or eval is provided
-            - method='save_report', granularity='global', but no eval is provided
             - method='save_report', eval='test', but no granularity is provided
         """
+        rpt = Reporter(method='save_report', granularity='global')
+        assert isinstance(rpt.reporter, ReporterSaveReport) == True
+
         with pytest.raises(ConfigError):
             Reporter(method='save_report')
-        with pytest.raises(ConfigError):
-            Reporter(method='save_report', granularity='global')
         with pytest.raises(ConfigError):
             Reporter(method='save_report', eval='test')
 
@@ -78,9 +80,10 @@ class Test_ReporterSaveData:
             - a list of strings
         - ReporterSaveData will raise a ConfigError when `source` is set to:
             - didn't setting
-            - a float value
-            - a list containing a float value
-            - a tuple
+            - other non-str/List[str] format, e.g.
+                - a float value
+                - a list containing a float value
+                - a tuple
         """
         cfg = {}
         cfg['method'] = 'save_data'
@@ -152,4 +155,45 @@ class Test_ReporterSaveReport:
 
         with pytest.raises(ConfigError):
             cfg['granularity'] = ['global','columnwise']
+            ReporterSaveReport(config=cfg)
+
+    def test_eval(self):
+        """
+        Test case for the arg. `eval` of ReporterSaveReport class.
+
+        - ReporterSaveReport will be created when `eval` is set to:
+            - a string
+            - a list of strings
+            - didn't setting
+        - ReporterSaveReport will raise a ConfigError when `eval` is set to:
+            - other non-str/List[str] format, e.g.
+                - a float value
+                - a list containing a float value
+                - a tuple
+        """
+        cfg = {}
+        cfg['method'] = 'save_report'
+        cfg['granularity'] = 'global'
+
+        rpt = ReporterSaveReport(config=cfg)
+        assert isinstance(rpt, ReporterSaveReport) == True
+
+        cfg['eval'] = 'test'
+        rpt = ReporterSaveReport(config=cfg)
+        assert isinstance(rpt, ReporterSaveReport) == True
+
+        cfg['eval'] = ['test1','test2']
+        rpt = ReporterSaveReport(config=cfg)
+        assert isinstance(rpt, ReporterSaveReport) == True
+
+        with pytest.raises(ConfigError):
+            cfg['eval'] = 0.8
+            ReporterSaveReport(config=cfg)
+
+        with pytest.raises(ConfigError):
+            cfg['eval'] = ['test', 0.8]
+            ReporterSaveReport(config=cfg)
+
+        with pytest.raises(ConfigError):
+            cfg['eval'] = ('test1','test2')
             ReporterSaveReport(config=cfg)
