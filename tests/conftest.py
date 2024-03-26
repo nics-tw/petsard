f5d96313254a595a2247f6753b48b7b0b0d4f672
matheme-justyn <matheme.justyn@gmail.com>
2024-03-26 09:48:21 +0800

    20240326, create conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+import sys
+from pathlib import Path
+
+
+# locate PETsARD/PETsARD and add it to sys.path
+path_conftest = Path(__file__)
+path_petsard = path_conftest.parent.parent.joinpath('PETsARD')
+sys.path.append(path_petsard)
