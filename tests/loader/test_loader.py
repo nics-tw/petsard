26922067c9f528e6018e9c8a76d85c8d59c5be36
matheme-justyn <matheme.justyn@gmail.com>
2024-03-26 09:49:15 +0800

    20240326, create test_loader.py

---
+++ tests/loader/test_loader.py
@@ -0,0 +1,14 @@
+import pytest
+
+from PETsARD import Loader
+
+
+class Test_Loader:
+
+    def test_handle_filepath_with_complex_name(self):
+        # 5. extract file extension
+        #    - issue 375, with complex file name
+        test = 'Splitter[0.8][1-1][train].csv'
+        load = Loader(filepath=test)
+
+        assert load.config['file_ext'] == '.csv'
