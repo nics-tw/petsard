d010a850376d5c911bdb2bde17aeb98bf9bb75fa
matheme-justyn <matheme.justyn@gmail.com>
2024-04-07 21:56:11 +0800

    20240407, simplify only SDVSingleTable

--- PETsARD/synthesizer/sdv.py
+++ PETsARD/synthesizer/sdv.py
@@ -87,23 +87,7 @@ class SDVFactory:
         return self.synthesizer
 
 
-class SDV():
-    """
-    Base class for all "SDV".
-        The "SDV" class defines the common API
-        that all the "SDV" need to implement, as well as common functionality.
-
-    Args:
-        data (pd.DataFrame): The data to be synthesized.
-        **kwargs: The other parameters.
-    """
-
-    def __init__(self, data: pd.DataFrame, **kwargs):
-        self.data: pd.DataFrame = data
-        self.syn_method: str = 'Unknown'
-
-
-class SDVSingleTable(SDV):
+class SDVSingleTable():
     """
     Base class for all SDV SingleTable classes.
 
@@ -115,6 +99,8 @@ class SDVSingleTable(SDV):
 
     def __init__(self, data: pd.DataFrame, metadata=None, **kwargs) -> None:
         super().__init__(data, **kwargs)
+        self.data: pd.DataFrame = data
+        self.syn_method: str = 'Unknown'
 
         self._SingleTableMetadata(metadata)
 
