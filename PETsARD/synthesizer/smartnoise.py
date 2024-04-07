c3653e0471a0f29bfbd3cc26d5e2358271be6532
matheme-justyn <matheme.justyn@gmail.com>
2024-04-08 01:21:01 +0800

    20240408, Smartnoise intro SynthesizerBase: remove fit_sample()

--- PETsARD/synthesizer/smartnoise.py
+++ PETsARD/synthesizer/smartnoise.py
@@ -134,28 +134,6 @@ class SmartNoise(SyntheszierBase):
         else:
             raise UnfittedError
 
-    def fit_sample(
-            self,
-            sample_num_rows:  int = None,
-            reset_sampling:   bool = False,
-            output_file_path: str = None
-    ) -> pd.DataFrame:
-        """
-        Fit and sample from the synthesizer.
-        The combination of the methods `fit()` and `sample()`.
-        Args:
-            sample_num_rows (int, default=None):
-                Number of synthesized data will be sampled.
-            reset_sampling (bool, default=False):
-                Redundant variable.
-            output_file_path (str, default=None):
-                The location of the output file.
-        Return:
-            data_syn (pd.DataFrame): The synthesized data.
-        """
-        self.fit()
-        return self.sample(sample_num_rows, reset_sampling, output_file_path)
-
 
 class SmartNoiseFactory:
     """
