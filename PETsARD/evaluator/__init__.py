229c53a80b8ba1f68b3fda62504e3f4c07d0de7f
Alex Chen <alex@AlexdeMacBook-Air.local>
2024-04-15 15:40:01 +0800

    introduce new ML framework

--- PETsARD/evaluator/__init__.py
+++ PETsARD/evaluator/__init__.py
@@ -1,9 +1,11 @@
 from PETsARD.evaluator.evaluator import Evaluator
 from PETsARD.evaluator.describer import Describer
 from PETsARD.evaluator.automl import AutoML
+from PETsARD.evaluator.mlutlity import MLUtility
 
 __all__ = [
     'Evaluator',
     'Describer',
-    'AutoML'
+    'AutoML',
+    'MLUtility'
 ]
