a2edd02a4ac8c1cd7fcc59c3a160a5e1f95df431
matheme-justyn <matheme.justyn@gmail.com>
2024-04-18 11:20:20 +0800

    20240418, simplify StatsBase

--- PETsARD/evaluator/stats.py
+++ PETsARD/evaluator/stats.py
@@ -31,22 +31,8 @@ class StatsBase(ABC):
             TypeError: If the data type verification fails.
         """
         self.data = data
-        self._create()
-        self.verify: dict = {}
-        if self._verify_dtype():
-            self.verify['dtype'] = True
-        else:
+        if not self._verify_dtype():
             raise TypeError
-
-    @abstractmethod
-    def _create(self):
-        """
-        Performs the actual creation of statistics.
-
-        Raises:
-            NotImplementedError: If the method is not implemented.
-        """
-        raise NotImplementedError
 
     @abstractmethod
     def _verify_dtype(self) -> bool:
@@ -70,9 +56,7 @@ class StatsBase(ABC):
             (int | float): The result of the statistics evaluation.
         """
         result: int | float = self._eval()
-        if isinstance(result, float):
-            result = safe_round(result)
-        return result
+        return safe_round(result) if isinstance(result, float) else result
 
     @abstractmethod
     def _eval(self) -> int | float:
@@ -88,27 +72,10 @@ class StatsBase(ABC):
         raise NotImplementedError
 
 
-class StatsColumnwise(StatsBase):
-    """
-    A class for calculating column-wise statistics.
-        Inherits from StatsBase.
-    """
-
-    def __init__(self):
-        super().__init__()
-
-    def _create(self):
-        """
-        Attributes:
-            col (pd.Series): The column data.
-        """
-        self.col: pd.Series = self.data['col']
-
-
-class StatsMean(StatsColumnwise):
+class StatsMean(StatsBase):
     """
     A class of column-wise statistic for the mean.
-        Inherits from StatsColumnwise.
+        Inherits from StatsBase.
     """
 
     def _verify_dtype(self) -> bool:
@@ -116,41 +83,41 @@ class StatsMean(StatsColumnwise):
         Returns:
             (bool): True if the column's data type is numeric, False otherwise.
         """
-        return is_numeric_dtype(self.col)
+        return is_numeric_dtype(self.data['col'])
 
     def _eval(self) -> float:
         """
         Returns:
             (float): The mean value of the column.
         """
-        return self.col.mean()
+        return self.data['col'].mean()
 
 
-class StatsStd(StatsColumnwise):
+class StatsStd(StatsBase):
     """
     A class of column-wise statistic for the standard deviation.
-        Inherits from StatsColumnwise.
+        Inherits from StatsBase.
     """
 
-    def _verify_dtype(self,) -> bool:
+    def _verify_dtype(self) -> bool:
         """
         Returns:
             (bool): True if the column's data type is numeric, False otherwise.
         """
-        return is_numeric_dtype(self.col)
+        return is_numeric_dtype(self.data['col'])
 
     def _eval(self) -> float:
         """
         Returns:
             (float): The standard deviation of the column.
         """
-        return self.col.std()
+        return self.data['col'].std()
 
 
-class StatsNUnique(StatsColumnwise):
+class StatsNUnique(StatsBase):
     """
     A class of column-wise statistic for the number of unique values.
-        Inherits from the StatsColumnwise.
+        Inherits from the StatsBase.
     """
 
     def _verify_dtype(self) -> bool:
@@ -158,14 +125,14 @@ class StatsNUnique(StatsColumnwise):
         Returns:
             (bool): True if the data type is 'category', False otherwise.
         """
-        return self.col.dtype == 'category'
+        return self.data['col'].dtype == 'category'
 
     def _eval(self) -> int:
         """
         Returns:
             (int): The number of unique values in the column.
         """
-        return self.col.nunique(dropna=True)
+        return self.data['col'].nunique(dropna=True)
 
 
 class Stats(EvaluatorBase):
