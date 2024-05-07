4ff70520864fdfce356d5b612be9cdbc74f9f2ca
matheme-justyn <matheme.justyn@gmail.com>
2024-05-07 13:26:35 +0800

    feat: change pct_change to diff in automl

--- PETsARD/evaluator/automl.py
+++ PETsARD/evaluator/automl.py
@@ -479,9 +479,8 @@ class ML:
                                    'syn_std': safe_round(np.std(syn_value))},
                                   index=[0])
 
-        compare_df['pct_change'] = safe_round((compare_df['syn_mean'] -
-                                               compare_df['ori_mean']) /
-                                              normalise_range) * 100
+        compare_df['diff'] = safe_round(
+             compare_df['syn_mean'] - compare_df['ori_mean'])
 
         return compare_df
 
